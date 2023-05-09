140d7e6137c6faa04e80a91cc886075da3cb907c
Alexander Castillo <fcastillo@hightech-corp.com>
2023-05-09 17:27:23 -0600

    Ejercicio 1 completado

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -2,3 +2,16 @@ console.log("hello world");
 
 
 console.log("laater");
+
+// Crea un script en ese fichero con tres variables (elige un nombre descriptivo para
+// cada una). La primera almacenará tu nombre y apellidos, la segunda tu profesión y la
+// tercera el puesto que querrías desempeñar o el que desempeñas actualmente.
+
+let name = 'Alexander Castillo';
+let proffesion = 'Licenciado en Informatica';
+let job = 'Software Architect';
+
+let presentation = `Hola, me llamo ${name} y soy ${proffesion}. Estoy cursando este Máster y actualmente trabajo como ${job}`
+
+console.log(presentation);
+
